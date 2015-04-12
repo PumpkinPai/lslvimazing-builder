4040a85d1c32f881d96fbe9b9f30bfa63c69c8c9
pumpkinpai <pumpkinson@gmail.com>
2015-04-12 11:27:55 -0400

    First commit!

---
+++ buildit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+##############################################################################
+#
+# This little gem scrapes the official LSL wiki and builds a vim plugin with
+# that data.  Syntax definitions are added from the text files afterwards.
+#
+##############################################################################
+
+# Probably import beautifulsoup depending on how specific the wiki tags are
+
+import sockets
+
+
